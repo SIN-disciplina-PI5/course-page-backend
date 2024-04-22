75daec256faffd0bfe05f7a4ccbd44e44dc5434a
Paulo Barreto Neto <paulo.neto@vectracs.com.br>
2024-04-22 11:45:14 -0300

    Adição da entidade usuário

--- UNICAP.SiteCurso.Infrastructure/Entities/User.cs
+++ UNICAP.SiteCurso.Infrastructure/Entities/User.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace UNICAP.SiteCurso.Infrastructure.Entities
 {
-    internal class User
+    [Table("TB_USER")]
+    public class User : BaseEntity<int>
     {
+        [Column("NOME")]
+        [Required]
+        [MaxLength(255)]
+        [MinLength(3)]
+        public string Nome { get; set; }
+
+        [Column("EMAIL")]
+        [Required]
+        [MaxLength(255)]
+        [MinLength(3)]
+        public string Email { get; set; }
+
+        [Column("CREDENTIALS_ID")]
+        [Required]
+        [ForeignKey("Credentials")]
+        public int CredentialsId { get; set; }
+        public virtual UserCredentials Credentials { get; set; }
     }
 }
