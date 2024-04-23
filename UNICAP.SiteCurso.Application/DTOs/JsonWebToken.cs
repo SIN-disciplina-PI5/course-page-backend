c752b5c1ac23844a0b081f9ab77c442c7e4a8b99
Paulo Barreto Neto <paulo.neto@vectracs.com.br>
2024-04-23 16:56:15 -0300

    Criação dos DTOS de JWT e RefreshToken

---
+++ UNICAP.SiteCurso.Application/DTOs/JsonWebToken.cs
@@ -0,0 +1,9 @@
+﻿namespace UNICAP.SiteCurso.Application.DTOs
+{
+    public class JsonWebToken
+    {
+        public string AccessToken { get; set; }
+        public RefreshToken RefreshToken { get; set; }
+        public DateTime ExpiresIn { get; set; }
+    }
+}
