83e2d2135fa1df706969d2a5e1f248fc3a34ff70
Paulo Barreto Neto <paulo.neto@vectracs.com.br>
2024-04-30 20:31:11 -0300

    feature : add user mapper

---
+++ UNICAP.SiteCurso.Application/AutoMapper/UserMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using UNICAP.SiteCurso.Application.DTOs.UserFolder;
+using UNICAP.SiteCurso.Application.Extensions;
+using UNICAP.SiteCurso.Domain.Entities;
+
+namespace UNICAP.SiteCurso.Application.AutoMapper
+{
+    public class UserMapper : Profile
+    {
+        public UserMapper() 
+        {
+            CreateMap<User, UserDTO>()
+                .ForMember(dest => dest.Nome, opt => opt.MapFrom(src => src.Nome))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.Cargo, opt => opt.MapFrom(src => src.Credentials.Role))
+                .ForMember(dest => dest.DescricaoCargo, opt => opt.MapFrom(src => src.Credentials.Role.GetDescription()));
+        }
+    }
+}
