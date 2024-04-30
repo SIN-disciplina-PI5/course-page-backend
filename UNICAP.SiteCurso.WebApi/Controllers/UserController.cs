488b80e9fb32b31f196ef8590f6541226505106a
Paulo Barreto Neto <paulo.neto@vectracs.com.br>
2024-04-30 20:26:07 -0300

    feature : add endpoints on user controller

--- UNICAP.SiteCurso.WebApi/Controllers/UserController.cs
+++ UNICAP.SiteCurso.WebApi/Controllers/UserController.cs
@@ -5,7 +5,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using UNICAP.SiteCurso.Application.CQRS.UserFolder.Commands.Create;
+using UNICAP.SiteCurso.Application.CQRS.UserFolder.Commands.Delete;
+using UNICAP.SiteCurso.Application.CQRS.UserFolder.Commands.Update;
 using UNICAP.SiteCurso.Application.CQRS.UserFolder.Queries.GetAll;
+using UNICAP.SiteCurso.Application.CQRS.UserFolder.Queries.GetById;
 using UNICAP.SiteCurso.Application.DTOs.GenericsFolder;
 
 namespace UNICAP.SiteCurso.WebApi.Controllers
@@ -43,6 +46,44 @@ namespace UNICAP.SiteCurso.WebApi.Controllers
         }
 
         /// <summary>
+        /// Atualiza as informações de um usuário.
+        /// </summary>
+        /// <param name="request">Dados para atualização do usuário.</param>
+        /// <returns>Resposta da operação de atualização.</returns>
+        /// <response code="200">Usuário atualizado com sucesso.</response>
+        /// <response code="400">Dados inválidos ou não preenchidos.</response>
+        /// <response code="401">Esta funcionalidade requer autenticação.</response>
+        /// <response code="500">Ocorreu um erro interno do servidor.</response>
+        [HttpPut]
+        [Route("UpdateUser")]
+        [ProducesResponseType(typeof(Response), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Response), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(Response), StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(typeof(Response), StatusCodes.Status500InternalServerError)]
+        public async Task<Response> Update([FromBody] UpdateUserCommand request)
+        {
+            return await _mediator.Send(request);
+        }
+
+        /// <summary>
+        /// Desativa um usuário.
+        /// </summary>
+        /// <param name="request">Dados para desativação do usuário.</param>
+        /// <returns>Resposta da operação de desativação.</returns>
+        /// <response code="200">Usuário inativado com sucesso.</response>
+        /// <response code="401">Esta funcionalidade requer autenticação.</response>
+        /// <response code="500">Ocorreu um erro interno do servidor.</response>
+        [HttpDelete]
+        [Route("DeleteUser")]
+        [ProducesResponseType(typeof(Response), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Response), StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(typeof(Response), StatusCodes.Status500InternalServerError)]
+        public async Task<Response> Delete([FromQuery] DeleteUserCommand request)
+        {
+            return await _mediator.Send(request);
+        }
+
+        /// <summary>
         /// Obtém uma lista de todos os usuários.
         /// </summary>
         /// <param name="withDisabled">Indica se usuários desativados devem ser incluídos na lista.</param>
@@ -60,6 +101,24 @@ namespace UNICAP.SiteCurso.WebApi.Controllers
         public async Task<Response> GetAll([FromQuery] bool withDisabled)
         {
             return await _mediator.Send(new GetAllUserQuery(withDisabled));
+        }
+
+        /// <summary>
+        /// Obtém um usuário pelo seu ID.
+        /// </summary>
+        /// <param name="request">ID do usuário a ser obtido.</param>
+        /// <returns>Resposta contendo o usuário encontrado.</returns>
+        /// <response code="200">Usuário encontrado com sucesso.</response>
+        /// <response code="401">Esta funcionalidade requer autenticação.</response>
+        /// <response code="500">Ocorreu um erro interno do servidor.</response>
+        [HttpGet]
+        [Route("GetUserById")]
+        [ProducesResponseType(typeof(Response), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Response), StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(typeof(Response), StatusCodes.Status500InternalServerError)]
+        public async Task<Response> GetById([FromQuery] GetUserByIdQuery request)
+        {
+            return await _mediator.Send(request);
         }
     }
 }
